6b198126edfd61d941cf4b0798b71e058235a8f8
subhan2108 <sk1623969@gamil.com>
2025-09-27 22:16:32 +0530

    admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 from .models import CompanyInfo, Service, Project, Certification, Message
 from .models import *
 
-admin.site.register(CompanyInfo)
-admin.site.register(Service)
-admin.site.register(Project)
-admin.site.register(Certification)
-admin.site.register(Message)
+#admin.site.register(CompanyInfo)
+#admin.site.register(Service)
+#admin.site.register(Project)
+#admin.site.register(Certification)
+#admin.site.register(Message)
 admin.site.register(JobApplication)
+admin.site.register(ContactMessage)
